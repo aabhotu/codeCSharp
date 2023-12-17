c9d49b9792b4e0af759be5cc53ebdbbf29159d55
Thao <nguyenthithuy18012002@gmail.com>
2023-12-17 15:59:38 +0700

    attribute

--- learnMore/learnMore/Program.cs
+++ learnMore/learnMore/Program.cs
@@ -1,12 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 
+
+[AttributeUsage(AttributeTargets.Class | AttributeTargets.Method | AttributeTargets.Property)]
+public class MotaAttribute: Attribute
+{
+    public string Thongtinchitiet { get; set; }
+    public MotaAttribute(string _Thongtinchitiet)
+    {
+        Thongtinchitiet = _Thongtinchitiet;
+    }
+}
+
+[Mota("Lop chua thong tin nguoi dung")]
 public class User
 {
+    [Mota("ten nguoi dung")]
     public string Name { get; set; }
+    [Mota("TUoi nguoi dung")]
+    [Range(18, 80, ErrorMessage ="Tuoi khong hop le")]
     public int Age { get; set; }
+    [Phone]
     public string PhoneNumber { get; set; }
+    [EmailAddress]
     public string Email { get; set; }
 }
 
@@ -21,12 +39,27 @@ class Program
             Email = "nguyenthithuy@gmail.com"
         };
 
-        var properties = user.GetType().GetProperties();
-        foreach(var property in properties)
-        {
-            var name = property.Name;
-            var value = property.GetValue(user);
-            Console.WriteLine($"{name} : {value}");
+        ValidationContext context = new ValidationContext(user);
+        var result = new List<ValidationResult>();
+
+        bool kq = Validator.TryValidateObject(user, context, result, true);
+        if (kq==false) {
+            result.ToList().ForEach((er) =>
+            {
+                Console.WriteLine(er.ErrorMessage);
+            });
         }
+
+        //var properties = user.GetType().GetProperties();
+        //foreach(var property in properties)
+        //{
+        //    foreach(var attr in property.GetCustomAttributes(false)){
+        //        MotaAttribute mota = attr as MotaAttribute;
+        //        if(mota != null)
+        //        {
+        //            Console.WriteLine(mota.Thongtinchitiet);
+        //        }
+        //    };
+        //}
     }
 }
---
+++ learnMore/learnMore/attribute.cs
@@ -0,0 +1,67 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.ComponentModel.DataAnnotations;
+//using System.Linq;
+
+
+//[AttributeUsage(AttributeTargets.Class | AttributeTargets.Method | AttributeTargets.Property)]
+//public class MotaAttribute : Attribute
+//{
+//    public string Thongtinchitiet { get; set; }
+//    public MotaAttribute(string _Thongtinchitiet)
+//    {
+//        Thongtinchitiet = _Thongtinchitiet;
+//    }
+//}
+
+//[Mota("Lop chua thong tin nguoi dung")]
+//public class User
+//{
+//    [Mota("ten nguoi dung")]
+//    public string Name { get; set; }
+//    [Mota("TUoi nguoi dung")]
+//    [Range(18, 80, ErrorMessage = "Tuoi khong hop le")]
+//    public int Age { get; set; }
+//    [Phone]
+//    public string PhoneNumber { get; set; }
+//    [EmailAddress]
+//    public string Email { get; set; }
+//}
+
+//class Program
+//{
+//    static void Main(string[] args)
+//    {
+//        User user = new User()
+//        {
+//            Name = "ThaoNguyen",
+//            Age = 21,
+//            PhoneNumber = "0918775755",
+//            Email = "nguyenthithuy@gmail.com"
+//        };
+
+//        ValidationContext context = new ValidationContext(user);
+//        var result = new List<ValidationResult>();
+
+//        bool kq = Validator.TryValidateObject(user, context, result, true);
+//        if (kq == false)
+//        {
+//            result.ToList().ForEach((er) =>
+//            {
+//                Console.WriteLine(er.ErrorMessage);
+//            });
+//        }
+
+//        //var properties = user.GetType().GetProperties();
+//        //foreach(var property in properties)
+//        //{
+//        //    foreach(var attr in property.GetCustomAttributes(false)){
+//        //        MotaAttribute mota = attr as MotaAttribute;
+//        //        if(mota != null)
+//        //        {
+//        //            Console.WriteLine(mota.Thongtinchitiet);
+//        //        }
+//        //    };
+//        //}
+//    }
+//}
